5fbbfbff764fb3582e5115a92a3033cb88b019a9
DWyck2 <93880148+DWyck2@users.noreply.github.com>
2021-11-07 13:43:52 -0500

    Add files via upload

---
+++ Lab 7J/CalcOp.java
@@ -0,0 +1,6 @@
+public interface CalcOp {
+    float add(float x,float y);
+    float subtract(float x,float y);
+    float multiply(float x,float y);
+    float divide(float x,float y);
+}
---
+++ Lab 7J/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    float add(float x,float y){
+        return x+y;
+    }
+    float subtract(float x,float y){
+        return x-y;
+    }
+    float multiply(float x,float y){
+        return x*y;
+    }
+    float divide(float x,float y){
+        return x/y;
+    }
+}
